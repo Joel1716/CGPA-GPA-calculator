e56559e79308970ccd62533f06d7fada201fdbcc
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2025-03-20 02:05:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -12,10 +12,14 @@ function clicked(event) {
 }
 function inputVal() {
   let totalValue = 0; // Reset the value everytime u enter the function
-  for (let i = 0; i < totalGPa.length; i++) {
-    let stringValue = totalGPa[i];
-    totalValue += eval(stringValue);
-  }
+   totalGPa.forEach((value) => {
+    let eachGPA = value.split(",");
+    const sumOfEachGPA = eachGPA.reduce(
+      (total, num) => total + parseInt(num),
+      0
+    );
+    totalValue += sumOfEachGPA;
+  });
   return totalValue;
 }
 //// To get the unit value and add the individual;y
@@ -32,11 +36,14 @@ function unitClick(event) {
 }
 function getUnitValue() {
   let unit = 0; // Reset the value everytime u enter the function
-  for (let a = 0; a < unitArray.length; a++) {
-    let unitValue = unitArray[a];
-    let unitValues = eval(unitValue);
-    unit += unitValues;
-  }
+  unitArray.forEach((value) => {
+    let eachUnit = value.split(",");
+    const sumOfEachUnit = eachUnit.reduce(
+      (total, num) => total + parseInt(num),
+      0
+    );
+    unit += sumOfEachUnit;
+  });
   return unit;
 }
 /// To calculate for both of them and get the CGPA
@@ -59,14 +66,23 @@ function calc() {
 const GPA_Button = document.querySelector(".js-calc-GPA");
 GPA_Button.addEventListener("click", GPA);
 function GPA() {
-  let gpa = inputs.value;
-  let totalUnits = units.value;
-  let result = eval(gpa) / eval(totalUnits);
+  let eachGPA = inputs.value.split(",");
+  const gpaSum = eachGPA.reduce((total, num) => {
+    return total + Number(num);
+  }, 0);
+  let eachUnit = units.value.split(",");
+  const unitSum = eachUnit.reduce((total, num) => {
+    return total + Number(num);
+  }, 0);
+  let result = eval(gpaSum) / eval(unitSum);
   if (isNaN(result)) {
     errorMessage.innerHTML = "Complete the two Selections";
     errorMessage.classList.add("error-message");
   } else {
     result = (Math.round(result * 100) / 100).toFixed(2);
+    units.value = "";
+    inputs.value = "";
     document.querySelector(".js-display").innerHTML = result;
+    errorMessage.classList.remove("error-message");
   }
 }
